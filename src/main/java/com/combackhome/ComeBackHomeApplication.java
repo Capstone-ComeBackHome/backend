a694a4ea36ef23d0dd51e68c5d06c27d87c0ef14
backtony <cjs1863@gmail.com>
2022-03-09 16:49:18 +0900

    프로젝트 세팅

---
+++ src/main/java/com/combackhome/ComeBackHomeApplication.java
@@ -0,0 +1,13 @@
+package com.combackhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeBackHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComeBackHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/combackhome/ComeBackHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.combackhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComeBackHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

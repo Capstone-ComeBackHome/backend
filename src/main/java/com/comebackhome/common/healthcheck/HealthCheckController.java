355e22f9039c3b6d3c7402c7247ff6b6ed24be2f
backtony <cjs1863@gmail.com>
2022-03-17 23:52:15 +0900

    chore: jacoco 추가

--- src/main/java/com/comebackhome/common/healthcheck/HealthCheckController.java
+++ src/main/java/com/comebackhome/common/healthcheck/HealthCheckController.java
@@ -1,4 +1,4 @@
-package com.comebackhome.common;
+package com.comebackhome.common.healthcheck;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;

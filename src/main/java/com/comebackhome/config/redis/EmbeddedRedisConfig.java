549257e5c7bfdeac0b5d5e218f98f0fd3e7112a9
backtony <cjs1863@gmail.com>
2022-03-22 23:34:24 +0900

    rename: redis config 패키지 경로 수정

--- src/main/java/com/comebackhome/config/redis/EmbeddedRedisConfig.java
+++ src/main/java/com/comebackhome/config/redis/EmbeddedRedisConfig.java
@@ -1,4 +1,4 @@
-package com.comebackhome.config;
+package com.comebackhome.config.redis;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/comebackhome/config/redis/RedisCacheConfig.java
+++ src/main/java/com/comebackhome/config/redis/RedisCacheConfig.java
@@ -1,4 +1,4 @@
-package com.comebackhome.config;
+package com.comebackhome.config.redis;
 
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;

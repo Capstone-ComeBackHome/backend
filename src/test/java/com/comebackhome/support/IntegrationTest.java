9ce7967a82065ebd06d3dbbee8363838efc89866
backtony <cjs1863@gmail.com>
2022-03-22 17:18:00 +0900

    refactor: 로컬에서 사용하던 redis 대신 embedded redis로 전환

--- src/test/java/com/comebackhome/support/IntegrationTest.java
+++ src/test/java/com/comebackhome/support/IntegrationTest.java
@@ -1,15 +1,17 @@
 package com.comebackhome.support;
 
-import com.comebackhome.config.EmbeddedRedisConfig;
+import com.comebackhome.authentication.infrastructure.repository.LogoutAccessTokenRedisRepository;
+import com.comebackhome.authentication.infrastructure.repository.LogoutRefreshTokenRedisRepository;
+import com.comebackhome.user.infrastructure.repository.UserJpaRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-@Import(EmbeddedRedisConfig.class)
 @Transactional
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -17,4 +19,20 @@ public abstract class IntegrationTest {
 
     @Autowired protected MockMvc mockMvc;
     @Autowired protected ObjectMapper objectMapper;
+
+    @Autowired UserJpaRepository userJpaRepository;
+    @Autowired LogoutAccessTokenRedisRepository logoutAccessTokenRedisRepository;
+    @Autowired LogoutRefreshTokenRedisRepository logoutRefreshTokenRedisRepository;
+
+    protected String createJson(Object dto) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(dto);
+    }
+
+    @BeforeEach
+    void clearUser(){
+        userJpaRepository.deleteAllInBatch();
+        logoutAccessTokenRedisRepository.deleteAll();
+        logoutRefreshTokenRedisRepository.deleteAll();
+    }
+
 }

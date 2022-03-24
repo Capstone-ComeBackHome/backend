233bde870170316c3aab75ee583cdc13c37e1d78
backtony <cjs1863@gmail.com>
2022-03-24 20:53:07 +0900

    test: CalendarCommandService 중복 테스트 제거

--- src/test/java/com/comebackhome/unit/calendar/application/CalendarCommandServiceTest.java
+++ src/test/java/com/comebackhome/unit/calendar/application/CalendarCommandServiceTest.java
@@ -1,7 +1,6 @@
 package com.comebackhome.unit.calendar.application;
 
 import com.comebackhome.calendar.application.CalendarCommandService;
-import com.comebackhome.calendar.application.dto.ScheduleSaveRequestDto;
 import com.comebackhome.calendar.domain.repository.DiseaseTagRepository;
 import com.comebackhome.calendar.domain.repository.ScheduleDiseaseTagRepository;
 import com.comebackhome.calendar.domain.repository.ScheduleRepository;
@@ -12,15 +11,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
 import java.util.Optional;
 
-import static com.comebackhome.calendar.domain.DiseaseType.CUSTOM;
-import static com.comebackhome.calendar.domain.DiseaseType.HEAD;
-import static com.comebackhome.support.helper.CalendarGivenHelper.*;
+import static com.comebackhome.support.helper.CalendarGivenHelper.givenScheduleModifyRequestDto;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 
 @ExtendWith(MockitoExtension.class)
 public class CalendarCommandServiceTest {
@@ -29,53 +26,6 @@ public class CalendarCommandServiceTest {
     @Mock ScheduleRepository scheduleRepository;
     @Mock ScheduleDiseaseTagRepository scheduleDiseaseTagRepository;
     @Mock DiseaseTagRepository diseaseTagRepository;
-
-// todo
-//    @Test
-//    void CustomType_없는_스케줄_저장하기() throws Exception{
-//        //given
-//        ScheduleSaveRequestDto scheduleSaveRequestDto = givenScheduleSaveRequest().toScheduleSaveRequestDto(1L);
-//        scheduleSaveRequestDto.setDiseaseTagRequestDtoList(List.of(givenDiseaseTagRequestDto(HEAD,"두통")));
-//
-//        //when
-//        calendarCommandService.saveMySchedule(scheduleSaveRequestDto);
-//
-//        //then
-//        then(scheduleRepository).should().save(any());
-//        then(diseaseTagRepository).should().findDiseaseTagIdListByDiseaseTagNameList(any());
-//        then(diseaseTagRepository).should(never()).findDiseaseTagListByDiseaseTagNameList(any());
-//        then(diseaseTagRepository).should(never()).saveAll(any());
-//        then(scheduleDiseaseTagRepository).should().saveAll(any());
-//    }
-
-    @Test
-    void 이미_존재하는_Custom_Type_diseaseTag로_스케줄_저장하기() throws Exception{
-        //given
-        ScheduleSaveRequestDto scheduleSaveRequestDto = givenScheduleSaveRequestDto(1L);
-        given(scheduleRepository.save(any())).willReturn(1L);
-        given(diseaseTagRepository.findDiseaseTagListByDiseaseTagNameList(any()))
-                .willReturn(List.of(givenDiseaseTag(CUSTOM,"교통사고")));
-
-        //when
-        calendarCommandService.saveMySchedule(scheduleSaveRequestDto);
-
-        //then
-        then(scheduleDiseaseTagRepository).should().saveAll(any());
-    }
-
-    // todo
-//    @Test
-//    void 존재하지_않는_customType_diseaseTag_스케줄_저장하기() throws Exception{
-//        //given
-//        ScheduleSaveRequestDto scheduleSaveRequestDto = givenScheduleSaveRequestDto(1L);
-//        given(scheduleRepository.save(any())).willReturn(1L);
-//
-//        //when
-//        calendarCommandService.saveMySchedule(scheduleSaveRequestDto);
-//
-//        //then
-//        then(scheduleDiseaseTagRepository).should().saveAll(any());
-//    }
 
     @Test
     void scheduleId로_스케줄_삭제() throws Exception{
@@ -92,7 +42,7 @@ public class CalendarCommandServiceTest {
     }
 
     @Test
-    void 내_스케줄_중_존재하지_않는_스케줄의_scheduleId() throws Exception{
+    void 내_스케줄_중_존재하지_않는_스케줄의_scheduleId_삭제() throws Exception{
         //given
         given(scheduleRepository.existsByIdAndUserId(any(),any()))
                 .willReturn(false);
